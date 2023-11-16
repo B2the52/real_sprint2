4a4da5f022299ddea81b29cf6423dcb1184a0ac8
B2the52 <thies811@gmail.com>
2023-11-16 10:55:31 -0600

    Blog create

--- LawnCare/urls.py
+++ LawnCare/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about_us/', views.about_us, name='about_us'),
     path('blog/', views.blog, name='blog'),
+    path('blog/create/', views.BlogCreate.as_view(), name='blog_create'),
     path('review/create/', views.ReviewCreate.as_view(), name='review_create'),
     path('service_list/', views.ServiceListView.as_view(), name='service_list'),
     path('service_detail/<int:pk>', views.ServiceDetailView.as_view(), name='service_detail'),
